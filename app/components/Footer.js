d247a27d615481aa6e1fe25aefeedd51456505b8
Rajat Malviya <rajatmalviya1738@gmail.com>
2026-07-03 19:56:57 +0530

    social handles changes again

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -141,7 +141,7 @@ export default function Footer() {
             </h4>
             <div className="space-y-3">
               <a
-                href="tel:+61428895741"
+                href="tel:0489943211"
                 className="cursor-pointer flex items-center gap-2 text-sm text-[#9CA3AF] hover:text-[#E5E7EB] transition-colors"
               >
                 <svg
@@ -154,7 +154,7 @@ export default function Footer() {
                 >
                   <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.76 12a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.7 1h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.65a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z" />
                 </svg>
-                +61 428 895 741
+                0489943211
               </a>
               <a
                 href="mailto:hello@crestwavedigital.com"
